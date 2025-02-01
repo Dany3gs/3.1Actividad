9a4a929504306e808537b66bbfcd97d4fb2beecd
Dany3gs <dsmmgc@gmail.com>
2025-02-01 22:51:15 +0000

    Ejercicio 5 terminados

--- src/Ejercicio5/Main.java
+++ src/Ejercicio5/Main.java
@@ -4,4 +4,33 @@ package Ejercicio5;
 resta, opuesto e inverso de un número racional a imitación de la suma o resta de los números reales o enteros.*/
 
 public class Main {
+	public static void main(String[] args) {
+		
+		Racional a = new Racional(1,2);
+		Racional b = new Racional(2,3);
+		
+		Racional resultadoSuma = a.sumar(b);
+		Racional resultadoResta = a.restar(b);
+		
+		System.out.println("-----------Suma-------------");
+		System.out.println(resultadoSuma);
+		
+		System.out.println("");
+		
+		System.out.println("-----------Resta-------------");
+		System.out.println(resultadoResta);
+		
+		System.out.println("");
+		
+		System.out.println("-----------Opuesto de suma y resta-------------");
+		System.out.println(resultadoSuma.opuesto());
+		System.out.println(resultadoResta.opuesto());
+		
+		System.out.println("");
+		
+		System.out.println("-----------Inverso de suma y resta-------------");
+		System.out.println(resultadoSuma.inverso());
+		System.out.println(resultadoResta.inverso());
+		
+	}
 }
---
+++ src/Ejercicio5/Racional.java
@@ -0,0 +1,71 @@
+package Ejercicio5;
+
+public class Racional {
+	private int numerador;
+	private int denominador;
+	
+	public Racional(int numerador, int denominador) {
+		if (denominador == 0) {
+			throw new IllegalArgumentException("El denominador no puede ser cero");
+		}
+		this.numerador = numerador;
+		this.denominador = denominador;
+	}
+	
+	public int getNumerador() {
+		return numerador;
+	}
+	
+	public void setNumerador(int numerador) {
+		this.numerador = numerador;
+	}
+	
+	public int getDenominador() {
+		return denominador;
+	}
+	
+	public void setDenominador(int denominador) {
+		this.denominador = denominador;
+	}
+	
+	// Métodos para las operaciones
+	public Racional sumar(Racional otro) {
+		int nuevoNumerador = (this.numerador * otro.denominador) + (this.denominador * otro.numerador);
+		int nuevoDenominador = this.denominador * otro.denominador;
+		return new Racional(nuevoNumerador, nuevoDenominador);
+	}
+	
+	public Racional restar(Racional otro) {
+		// Implementación de la resta
+		int nuevoNumerador = (this.numerador * otro.denominador) - (this.denominador * otro.numerador);
+		int nuevoDenominador = this.denominador * otro.denominador;
+		return new Racional(nuevoNumerador, nuevoDenominador);
+	}
+	
+	public Racional opuesto() {
+		// Por convención se debe mantener el denominador en positivo, se indica en método para hacer referencia a su
+		// valor en opuesto
+		
+		return new Racional(-this.numerador, -this.denominador);
+		
+		/*Otra forma de hacerlo
+ 		int numeradorOpuesto = Math.negateExact(this.numerador);
+		int denominadorOpuesto = this.denominador;
+		return new Racional(numeradorOpuesto, denominadorOpuesto);
+		* */
+	}
+	
+	public Racional inverso() {
+		int numeradorInverso = this.denominador;
+		int denominadorInverso = this.numerador;
+		return new Racional(numeradorInverso, denominadorInverso);
+	}
+	
+	@Override
+	public String toString() {
+		return "Racional{" +
+				"numerador=" + numerador +
+				", denominador=" + denominador +
+				'}';
+	}
+}
